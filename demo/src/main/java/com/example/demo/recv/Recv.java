6910a425d231bfdbc9ec44362babc3d17d6764dd
zhanglei <zhanglei-itsh@bestpay.com.cn>
2018-07-02 10:56:19 +0800

    测试一下分支

---
+++ demo/src/main/java/com/example/demo/recv/Recv.java
@@ -0,0 +1,27 @@
+package com.example.demo.recv;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Recv {
+    private final static String QUEUE_NAME="hello";
+    public static void main(String[] arg) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        System.out.println("[*]等待消息。要退出按CTRL + C");
+        DefaultConsumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+            String message= new String (body,"UTF-8");
+            System.out.println(" [x] Received' "  + message +  " ' ");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME,true,consumer);
+    }
+
+
+}
